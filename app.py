a581d79b31f95640323a844c92bedb7bef35422e
nnfxbot <89951109+nnfxbot@users.noreply.github.com>
2023-07-21 06:47:21 +1000

    Update app.py

--- app.py
+++ app.py
@@ -17,16 +17,17 @@ sys_msg = """Role: Expert coder
 Tone: Casual
 Format: Bullet points"""
 
+
+
 with st.sidebar:
-    st.image("Logo.png")
     st.session_state.system_message = st.text_area("System message", height = 150, value = sys_msg)
     if st.button("Start new chat"):
         st.session_state.messages = [{"role":"system", "content":st.session_state.system_message}]
     st.session_state.openai_model = st.selectbox("Model", options = ["gpt-3.5-turbo", "gpt-3.5-turbo-16k"])
     st.session_state.max_history = st.slider("Max History", 2, 10, 2, 1)
-    st.markdown("")
+    st.image("Logo.png", width = 250)
     st.markdown(">## Created by AC ")
-
+    
 
 st.title("Agent C")
 
@@ -36,43 +37,46 @@ if "messages" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
 #Display messages in chat history
-for message in st.session_state.messages:
+messages = st.session_state.messages
+for message in messages:
     if message["role"] not in ["system", "function"]:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
 #Handle user input
 if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
         response = openai.ChatCompletion.create(
             model=st.session_state["openai_model"],
-            messages=st.session_state.messages,
+            messages=messages,
             functions = functions,
             function_call = "auto")
-        while response.choices[0].finish_reason == "function_call":
+        while response.choices[0].finish_reason == "function_call" or len(messages) > 10:
             function_call = response.choices[0].message.function_call
             st.markdown(function_call)
             result = handle_function_call(function_call)
             st.json(result, expanded = False)
-            st.session_state.messages.append({"role":"function", "name": function_call.name,
+            messages.append({"role":"function", "name": function_call.name,
                 "content": json.dumps(result)})
             response = openai.ChatCompletion.create(
                 model=st.session_state["openai_model"],
-                messages=st.session_state.messages,
+                messages=messages,
                 functions = functions,
                 function_call = "auto")   
         message = response.choices[0].get("message")
         st.markdown(message.content)
         st.json(response, expanded = False)
-        st.session_state.messages.append(message)
+        messages.append(message)
 
 #Keep the last messages according to max history limit
-if len(st.session_state.messages) > st.session_state.max_history:
-    st.session_state.messages = [st.session_state.messages[0]] + st.session_state.messages[-st.session_state.max_history:]
+max_history = st.session_state.max_history 
+if len(messages) > max_history:
+    messages = [messages[0]] + messages[-max_history:]
+st.session_state.messages = messages
 
 streamlit_analytics.stop_tracking()
 
